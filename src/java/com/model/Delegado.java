1bda7f8c5ecc7ee5891fc616e20975da64bd0fff
MisterRato <misterato@gmail.com>
2017-10-16 20:38:36 -0200

    Correções do back-end

---
+++ src/java/com/model/Delegado.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.model;
+
+/**
+ *
+ * @author Giovane
+ */
+import javax.persistence.*;  
+  
+@Entity  
+@DiscriminatorValue("delegado") 
+public class Delegado extends Pessoa{
+    
+}
---
+++ src/java/com/model/Usuario.java
@@ -0,0 +1,78 @@
+package com.model;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Giovane
+ */
+import java.io.Serializable;
+import java.math.BigInteger;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+   
+@Entity
+@Table(name = "usuario")  
+public class Usuario implements Serializable {
+       
+    
+    @Id  
+    @GeneratedValue(strategy=GenerationType.AUTO)    
+    @Column(name = "id") 
+    private Integer id;
+    @Column(name = "usuario") 
+    private String usuario;
+    @Column(name = "senha") 
+    private String senha;
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the usuario
+     */
+    public String getUsuario() {
+        return usuario;
+    }
+
+    /**
+     * @param usuario the usuario to set
+     */
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+}
