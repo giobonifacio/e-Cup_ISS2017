c7f23243dcc804a1a1d5da323e94f526d6eca446
giovanebonifacio <bonifacio.gio@gmail.com>
2017-10-22 23:45:50 -0200

    Resolvendo conflitos - Mari

---
+++ eCup2017/src/java/com/model/IntegranteEquipe.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.model;
+import javax.persistence.*;  
+  
+@Entity  
+public abstract class IntegranteEquipe extends Pessoa{
+    
+
+    @Column(name = "equipe")  
+    @ManyToOne
+    private Equipe equipe;    
+    @Column(name = "funcao")  
+    private String funcao;
+    
+    
+    
+}
+
+/**
+ *
+ * @author Ricardo
+ */
+
